38061ad65d0bf120dae9240e9ef8ee4309ad2b34
Jonathan Matarazzi <git@jonathanm.nl>
2023-12-05 23:03:13 +0100

    Fix acitivity animation

--- src/slides/Activities.vue
+++ src/slides/Activities.vue
@@ -6,28 +6,29 @@
       <h2>Which cost you a total of <p class="amount-spent"> €{{ stats.spent }}!</p></h2>
     </div>
     <div class="activity-container">
-      <div v-for="(activity, idx) in activities" class="move-up" :style="`animation-delay: ${idx/(activities.length)*20 - 5}s`">
-        <div class="activity" :style="`background-image: url(${activity.image_url})`">
+      <div v-for="(activity, idx) in activities" class="move-up" :style="`animation-delay: ${(idx-5)*(delay)}s`">
+        <div class="activity"
+             :style="`background-image: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${activity.image_url})`">
           <div class="title">
-            {{activity.title}}
+            {{ activity.title }}
           </div>
           <div class="footer">
             <div>
-              {{activity.formatted_date.simple}}
+              {{ activity.formatted_date.simple }}
             </div>
             <div>
-              {{activity.location}}
+              {{ activity.location }}
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="viewport">
-    </div>
   </div>
 </template>
 
 <script setup>
+const delay = 1;
+
 const props = defineProps({
   data: {
     required: true
@@ -38,46 +39,47 @@ const activities = props.data.wrapped.events
 </script>
 
 <style scoped>
-.viewport{
-  position: absolute;
-  left: .5em;
-  top: 14em;
-  width: 28em;
-  height: 33em;
-  border-radius: 3em;
-  box-shadow: 0 0 0 99999px deepskyblue;
-}
+
 .slide {
   background: linear-gradient(149deg, #BF1363 0%, #F39237 49%, rgb(42, 40, 100) 98%);
   text-align: center;
 }
 
-.amount-spent{
-  color:red;
+.amount-spent {
+  color: red;
   display: inline-block;
 }
 
 .activity-container {
-  height:100%
+  background: rgba(170,170,170,0.5);
+  border-radius: 2rem;
+  overflow: hidden;
+  margin-top: 3rem;
+  height: 35rem;
 }
-.move-up{
+
+.move-up {
   position: absolute;
-  animation:move 20s infinite linear;
+  animation: move infinite linear;
+  animation-duration: v-bind('`${delay*activities.length}s`');
   bottom: 0;
   opacity: 1;
   text-align: start;
   text-wrap: none;
-  width: 100%
+  width: 100%;
+  transform: translateY(7rem);
+  padding: 0 1rem;
 }
-.activity{
+
+.activity {
   text-align: start;
-  padding-left: .3em;
-  padding-right: .3em;
-  font-size: 1.5em;
-  border-radius: .5em;
-  height: 3em;
+  padding: 0 .3rem;
+  font-size: 1.5rem;
+  border-radius: .5rem;
+  height: 5rem;
   display: flex;
-  margin-bottom: .5em;
+  justify-content: space-evenly;
+  margin-bottom: .5rem;
   width: 100%;
   background-repeat: no-repeat;
   background-size: cover;
@@ -86,18 +88,19 @@ const activities = props.data.wrapped.events
 }
 
 
-.title{
+.title {
   text-wrap: none;
   overflow: hidden;
 }
-.content{
+
+.content {
   z-index: 2;
   border-radius: .5em;
   margin-top: 3em;
 }
 
-.footer{
-  color:lightblue;
+.footer {
+  color: lightblue;
   font-size: .8em;
   display: flex;
   justify-content: space-between;
@@ -105,11 +108,11 @@ const activities = props.data.wrapped.events
 
 @keyframes move {
   0% {
-transform:translateY(0);
+    transform: translateY(5rem);
   }
 
   100% {
-    transform:translateY(-350rem);
+    transform: v-bind('`translateY(min(-35rem, ${-5*1.5*activities.length}rem))`');
   }
 }
 </style>
